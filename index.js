e6cfb4897323b3f370b455bd035731d0c80a2f76
cowilliams_deloitte <cowilliams@deloitte.com>
2024-09-18 16:52:50 -0600

    getting started

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-const canvas = d3.select('.canvas');
+const data = [{ width: 200, height: 100, fill: 'purple' }];
 
-const svg = canvas.append('svg').attr('width', 600).attr('height', 600);
+const svg = d3.select('svg');
 
-const group = svg.append('g').attr('transform', translate(50, 100));
-
-group
-  .append('rect')
-  .attr('width', 200)
-  .attr('height', 100)
-  .attr('fill', 'blue')
-  .attr('x', 20)
-  .attr('y', 20);
-
-group.append('circle').attr('r', 50).attr('cx', 300).attr('cy', 70).attr('fill', 'pink');
-
-group
-  .append('text')
-  .attr('x', 20)
-  .attr('y', 200)
-  .attr('color', 'red')
-  .text('Hello, ninjas')
-  .style('font-family', 'arial');
-
-group
-  .append('line')
-  .attr('x1', 370)
-  .attr('x2', 400)
-  .attr('y1', 20)
-  .attr('y2', 120)
-  .attr('stroke', 'red');
+const rect = svg
+  .select('rect')
+  .data(data)
+  .attr('width', function (d) {
+    return d.width;
+  })
+  .attr('height', (d) => d.height)
+  .attr('fill', (d) => d.fill);
+console.log(rect);

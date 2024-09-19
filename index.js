770c0fa4c9d130c0c30c60b376f6ee76c88365c9
cowilliams_deloitte <cowilliams@deloitte.com>
2024-09-19 11:59:58 -0600

    working on removing hardcoded data

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const svg = d3.select('svg');
 
 d3.json('menu.json').then((data) => {
-  const y = d3.scaleLinear().domain([0, 1000]).range([0, 500]);
+  const min = d3.min(data, (d) => d.orders);
+  const max = d3.max(data, (d) => d.orders);
+  const extent = d3.extent(data, (d) => d.orders);
+
+  console.log('min:', min, 'max:', max, 'extent:', extent);
+
+  const y = d3.scaleLinear().domain([0, max]).range([0, 500]);
   const x = d3
     .scaleBand()
     .domain(data.map((item) => item.name))
-    .range([0, 500]);
+    .range([0, 500])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
 
   console.log(data.map((item) => item.name));
   console.log(x('cutlets'), x('filet'));
@@ -18,7 +26,7 @@ d3.json('menu.json').then((data) => {
     .attr('width', x.bandwidth)
     .attr('height', (d) => y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d, i) => (i + 1) * 70);
+    .attr('x', (d) => x(d.name));
 
   rects
     .enter()
@@ -26,5 +34,5 @@ d3.json('menu.json').then((data) => {
     .attr('width', x.bandwidth)
     .attr('height', (d) => y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d, i) => (i + 1) * 70);
+    .attr('x', (d) => x(d.name));
 });

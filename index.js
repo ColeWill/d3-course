99a20363262de4ac2fd5dfdbf3395c2fc2e11d10
cowilliams_deloitte <cowilliams@deloitte.com>
2024-09-20 12:34:38 -0600

    chart is facing the right way

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const svg = d3.select('svg');
+const svg = d3.select('.canvas').append('svg').attr('width', 600).attr('height', 600);
+
+// create margins and dimensions
+
+const margin = { top: 20, right: 20, bottom: 100, left: 100 };
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg
+  .append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+const xAxisGroup = graph.append('g').attr('transform', `translate(0, ${graphHeight})`);
+const yAxisGroup = graph.append('g');
 
 d3.json('menu.json').then((data) => {
   const min = d3.min(data, (d) => d.orders);
   const max = d3.max(data, (d) => d.orders);
   const extent = d3.extent(data, (d) => d.orders);
 
-  console.log('min:', min, 'max:', max, 'extent:', extent);
-
-  const y = d3.scaleLinear().domain([0, max]).range([0, 500]);
+  const y = d3.scaleLinear().domain([0, max]).range([graphHeight, 0]);
   const x = d3
     .scaleBand()
     .domain(data.map((item) => item.name))
@@ -15,24 +28,28 @@ d3.json('menu.json').then((data) => {
     .paddingInner(0.2)
     .paddingOuter(0.2);
 
-  console.log(data.map((item) => item.name));
-  console.log(x('cutlets'), x('filet'));
-
-  console.log(y(400));
-
-  const rects = svg.selectAll('rect').data(data);
+  const rects = graph.selectAll('rect').data(data);
 
   rects
     .attr('width', x.bandwidth)
-    .attr('height', (d) => y(d.orders))
+    .attr('height', (d) => graphHeight - y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d) => x(d.name));
+    .attr('x', (d) => x(d.name))
+    .attr('y', (d) => y(d.orders));
 
   rects
     .enter()
     .append('rect')
     .attr('width', x.bandwidth)
-    .attr('height', (d) => y(d.orders))
+    .attr('height', (d) => graphHeight - y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d) => x(d.name));
+    .attr('x', (d) => x(d.name))
+    .attr('y', (d) => y(d.orders));
+
+  // create and call the axes
+  const xAxis = d3.axisBottom(x);
+  const yAxis = d3.axisLeft(y);
+
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
 });

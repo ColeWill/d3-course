b27e393c8882209d9cc2fc2e494f57c9749839c0
cowilliams_deloitte <cowilliams@deloitte.com>
2024-09-23 13:20:11 -0600

    removed JS modules to make firebase available globally

--- index.js
+++ index.js
@@ -15,50 +15,53 @@ const graph = svg
 const xAxisGroup = graph.append('g').attr('transform', `translate(0, ${graphHeight})`);
 const yAxisGroup = graph.append('g');
 
-d3.json('menu.json').then((data) => {
-  const min = d3.min(data, (d) => d.orders);
-  const max = d3.max(data, (d) => d.orders);
-  const extent = d3.extent(data, (d) => d.orders);
+db.collection('dishes')
+  .get()
+  .then((res) => {
+    console.log('res', res);
+    // const min = d3.min(data, (d) => d.orders);
+    // const max = d3.max(data, (d) => d.orders);
+    // const extent = d3.extent(data, (d) => d.orders);
 
-  const y = d3.scaleLinear().domain([0, max]).range([graphHeight, 0]);
-  const x = d3
-    .scaleBand()
-    .domain(data.map((item) => item.name))
-    .range([0, 500])
-    .paddingInner(0.2)
-    .paddingOuter(0.2);
+    // const y = d3.scaleLinear().domain([0, max]).range([graphHeight, 0]);
+    // const x = d3
+    //   .scaleBand()
+    //   .domain(data.map((item) => item.name))
+    //   .range([0, 500])
+    //   .paddingInner(0.2)
+    //   .paddingOuter(0.2);
 
-  const rects = graph.selectAll('rect').data(data);
+    // const rects = graph.selectAll('rect').data(data);
 
-  rects
-    .attr('width', x.bandwidth)
-    .attr('height', (d) => graphHeight - y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x', (d) => x(d.name))
-    .attr('y', (d) => y(d.orders));
+    // rects
+    //   .attr('width', x.bandwidth)
+    //   .attr('height', (d) => graphHeight - y(d.orders))
+    //   .attr('fill', 'orange')
+    //   .attr('x', (d) => x(d.name))
+    //   .attr('y', (d) => y(d.orders));
 
-  rects
-    .enter()
-    .append('rect')
-    .attr('width', x.bandwidth)
-    .attr('height', (d) => graphHeight - y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x', (d) => x(d.name))
-    .attr('y', (d) => y(d.orders));
+    // rects
+    //   .enter()
+    //   .append('rect')
+    //   .attr('width', x.bandwidth)
+    //   .attr('height', (d) => graphHeight - y(d.orders))
+    //   .attr('fill', 'orange')
+    //   .attr('x', (d) => x(d.name))
+    //   .attr('y', (d) => y(d.orders));
 
-  // create and call the axes
-  const xAxis = d3.axisBottom(x);
-  const yAxis = d3
-    .axisLeft(y)
-    .ticks(4)
-    .tickFormat((d) => d + ' orders');
+    // // create and call the axes
+    // const xAxis = d3.axisBottom(x);
+    // const yAxis = d3
+    //   .axisLeft(y)
+    //   .ticks(4)
+    //   .tickFormat((d) => d + ' orders');
 
-  xAxisGroup.call(xAxis);
-  yAxisGroup.call(yAxis);
+    // xAxisGroup.call(xAxis);
+    // yAxisGroup.call(yAxis);
 
-  xAxisGroup
-    .selectAll('text')
-    .attr('transform', 'rotate(-40)')
-    .attr('text-anchor', 'end')
-    .attr('fill', 'orange');
-});
+    // xAxisGroup
+    //   .selectAll('text')
+    //   .attr('transform', 'rotate(-40)')
+    //   .attr('text-anchor', 'end')
+    //   .attr('fill', 'orange');
+  });
